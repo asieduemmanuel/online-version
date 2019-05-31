d146759111d97980106888346306bac2e500a31f
asieduemmanuel <38865826+asieduemmanuel@users.noreply.github.com>
2019-05-31 09:39:05 -0600

    Updated by Mannie

--- online version/Scripts/sideBar.js
+++ online version/Scripts/sideBar.js
@@ -5,10 +5,11 @@
     });
 
 });
-function toggle_div_fun(id) {
-    var divelement = document.getElementById(id);
-    if (divelement.style.display == 'none')
-        divelement.style.display = 'block';
-    else
-        divelement.style.display = 'none';
-}
+//function toggle_div_fun(id) {
+//    var divelement = document.getElementById(id);
+//    if (divelement.style.display == 'none')
+//        divelement.style.display = 'block';
+//    else
+//        divelement.style.display = 'none';
+//}
+//
